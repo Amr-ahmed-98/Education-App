32693e5b2fca204209971866b868713b80726cdd
Amr-ahmed-98 <67926050+Amr-ahmed-98@users.noreply.github.com>
2025-09-10 20:09:59 +0300

    Added New Branch

--- src/assets/images/images.ts
+++ src/assets/images/images.ts
@@ -1,8 +1,10 @@
 import HomeHero from "./Home-HeroSection.png";
 import HomeAbout from "./Home-AboutSection.png";
 import intructor1 from './instructor1.png';
+import AboutImg from './front-view-smiley-man-holding-paper 1.png';
 export{
     HomeHero,
     HomeAbout,
-    intructor1
+    intructor1,
+    AboutImg
 }
