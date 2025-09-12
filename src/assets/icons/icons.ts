7439002c890fb9c2de62900991a0ee99b425faf2
elsam-mohamedl <ee.eslam355@gmail.com>
2025-09-13 01:55:45 +0300

    add Section Footer

--- src/assets/icons/icons.ts
+++ src/assets/icons/icons.ts
@@ -1,12 +1,11 @@
 import { GlobeAltIcon } from "@heroicons/react/24/outline";
  
-import { AlignLeft , X , Moon ,SunMedium , Facebook , Angry, Clock, BookOpen, Star, Users  ,Check   } from 'lucide-react';
+import { AlignLeft , X , Moon ,SunMedium , Facebook , Angry, Clock, BookOpen, Star,Users,Check,Linkedin,Instagram } from 'lucide-react';
   
 import { IoLogoFacebook } from "react-icons/io5";
-import {FaInstagramSquare,FaWhatsappSquare,FaLinkedin, } from "react-icons/fa";
+import {FaInstagramSquare,FaWhatsappSquare,FaLinkedin,FaWhatsapp ,FaInstagram ,FaFacebookF ,FaLinkedinIn     } from "react-icons/fa";
 import { CiCalendarDate  ,CiLocationOn} from "react-icons/ci";
 import { TbClockHour4 } from "react-icons/tb";
-
 export {
   GlobeAltIcon,
   AlignLeft,
@@ -27,6 +26,9 @@ export {
   Clock,
   BookOpen,   
   Users,
-
+  FaWhatsapp,
+  FaInstagram,
+  FaFacebookF,
+  FaLinkedinIn 
 };
 
--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,8 +1,50 @@
-
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import * as icon from '../../assets/icons/icons';
 function Footer() {
+  const { t } = useTranslation();
   return (
-    <div>Footer</div>
-  )
+    <section className="bg-[#052861] ">
+      <div className="container py-[80px]  grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16">
+       <div>
+        <Link to="/" className="text-primary text-[30px]">Skill<span className="text-secondary">Base</span></Link>
+        <p className="mt-[20px] text-[18px]">{t("footer.description")}</p>
+        <div className="flex flex-row mt-[20px]">
+          <Link className="w-[50px] h-[50px] mr-[7px]  flex items-center justify-center rounded-[50%] bg-[#1C4281]" to="/"><icon.FaLinkedinIn size={24} /></Link>
+          <Link className="w-[50px] h-[50px] mr-[7px]  flex items-center justify-center rounded-[50%] bg-[#1C4281]" to="/"><icon.FaInstagram size={24} /></Link>
+          <Link className="w-[50px] h-[50px] mr-[7px]  flex items-center justify-center rounded-[50%] bg-[#1C4281]" to="/"><icon.FaWhatsapp size={24} /></Link>
+          <Link className="w-[50px] h-[50px] mr-[7px]  flex items-center justify-center rounded-[50%] bg-[#1C4281]" to="/"><icon.FaFacebookF size={24} /></Link>
+
+        </div>
+       </div>
+       <div className="text-secondary text-[30px]">
+         <h2 className="mb-[20px]">{t("footer.servicesTitle")}</h2>
+         <ul className="text-light-primary text-[20px]">
+          <li >{t("footer.services.dataScience")}</li>
+          <li>{t("footer.services.leadership")}</li>
+          <li>{t("footer.services.webDevelopment")}</li>
+          <li>{t("footer.services.communication")}</li>
+          <li>{t("footer.services.marketing")}</li>
+         </ul>
+       </div>
+
+       <div className="text-secondary text-[30px]">
+         <h2 className="mb-[20px]">{t("footer.linksTitle")}</h2>
+         <ul className="text-light-primary text-[20px] flex flex-col">
+          <Link to="/" >{t("footer.links.pages")}</Link>
+          <Link to="/" >{t("footer.links.blogs")}</Link>
+          <Link to="/" >{t("footer.links.events")}</Link>
+          <Link to="/" >{t("footer.links.courses")}</Link>
+          <Link to="/" >{t("footer.links.faqs")}</Link>
+         </ul>
+       </div>
+      </div>
+      <div className="copyright py-[15px] text-center w-full bg-[#d9d9d92c]">
+          <p className="text-[20px]">{t("footer.copyright")}</p>
+      </div>
+    </section>
+
+    )
 }
 
 export default Footer
