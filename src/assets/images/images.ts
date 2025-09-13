f918233ee7cfed9e0094939242d0c3cc4b638d38
Amr-ahmed-98 <67926050+Amr-ahmed-98@users.noreply.github.com>
2025-09-13 16:07:39 +0300

    uploaded Contact Section

--- src/assets/images/images.ts
+++ src/assets/images/images.ts
@@ -1,7 +1,7 @@
 import HomeHero from "./Home-HeroSection.png";
 import HomeAbout from "./Home-AboutSection.png";
 import intructor1 from "./instructor1.png";
-import AboutImg from "./front-view-smiley-man-holding-paper 1.png";
+
 import EventDetailsHero from "./event-details-hero.png";
 import EventDetailsSpeaker1 from "./event-details-speaker(1).png";
 import EventDetailsSpeaker2 from "./event-details-speaker(2).png";
@@ -11,7 +11,6 @@ export {
   HomeHero,
   HomeAbout,
   intructor1,
-  AboutImg,
   EventDetailsHero,
   EventDetailsSpeaker1,
   EventDetailsSpeaker2,
