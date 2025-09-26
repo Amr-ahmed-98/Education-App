21701d7316d1ff5d7dcd5a757d736bf6a3c33fc4
MostafaYahya773 <mostafayahya822@gmail.com@users.noreply.github.com>
2025-09-26 18:10:25 +0300

    finish cource details taps

---
+++ src/components/organisms/LearningTable.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { FileVideoCamera, LockKeyhole } from 'lucide-react';
+interface WeekLearning {
+  weekLearn: string[];
+  time: string;
+  description: string;
+}
+export default function LearningTable({
+  weekLearn,
+  time,
+  description,
+}: WeekLearning) {
+  return (
+    <div>
+      <div className="border border-[#D9D9D9] dark:border-[#64748B] mt-10 flex flex-col p-6  ">
+        <h2 className="text-[16px] sm:text-[18px] ">{time}</h2>
+        <p className="my-5 border-b pb-2 border-[#D5D5D5] dark:border-[#64748B]  text-[#777777]">
+          {description}
+        </p>
+        <ul>
+          {weekLearn?.map((point, index) => (
+            <li
+              key={index}
+              className="flex items-start my-3 border-b space-x-2 border-[#D5D5D5] dark:border-[#64748B] pb-3"
+            >
+              <div className="flex-shrink-0 w-5 h-5  rounded-full flex items-center justify-center mt-0.5">
+                <FileVideoCamera className="w-5 h-5 dark:text-[#64748B]" />
+              </div>
+              <p className="text-black sm:text-[14px] dark:text-[#64748B]">
+                {point}
+              </p>
+              <div className="flex-shrink-0 w-5 h-5 dark:text-[#64748B] rounded-full flex items-center justify-center mt-0.5 ms-auto">
+                <LockKeyhole className="w-5 h-5 " />
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
--- src/components/pages/CourseDetails.tsx
+++ src/components/pages/CourseDetails.tsx
@@ -1,11 +1,11 @@
-import { useTranslation } from "react-i18next";
-import SectionTitle from "../molecules/SectionTitle";
-import CourseDetailsContent from "../molecules/CourseDetailsContent";
-import CourseDetailsSidebar from "../molecules/CourseDetailsSidebar";
-import { themes } from "@/contexts/Theme";
-import { ThemeContext } from "@/contexts/ThemeContextDefinition";
-import { useContext } from "react";
-import { useState } from "react";
+import { useTranslation } from 'react-i18next';
+import SectionTitle from '../molecules/SectionTitle';
+import CourseDetailsContent from '../molecules/CourseDetailsContent';
+import CourseDetailsSidebar from '../molecules/CourseDetailsSidebar';
+import { themes } from '@/contexts/Theme';
+import { ThemeContext } from '@/contexts/ThemeContextDefinition';
+import { useContext } from 'react';
+import { useState } from 'react';
 import {
   Clock,
   BookOpen,
@@ -13,41 +13,41 @@ import {
   Globe,
   Award,
   User,
-} from "@/assets/icons/icons";
+} from '@/assets/icons/icons';
 
 function CourseDetails() {
   const { t, i18n } = useTranslation();
   const { theme } = useContext(ThemeContext)!;
-  const isArabic = i18n.language === "ar";
-  const [activeTab, setActiveTab] = useState("Overview");
+  const isArabic = i18n.language === 'ar';
+  const [activeTab, setActiveTab] = useState('Overview');
 
   return (
     <div
       className={`${
-        theme === themes.dark ? "bg-dark-900" : "bg-gray-50"
+        theme === themes.dark ? 'bg-dark-900' : 'bg-gray-50'
       } min-h-screen`}
     >
       <SectionTitle
-        breadcrumbs1={t("SectionTitle.CourseDetails.breadcrumbs1")}
-        breadcrumbs2={t("SectionTitle.CourseDetails.breadcrumbs2")}
-        breadcrumbs3={t("SectionTitle.CourseDetails.breadcrumbs3")}
+        breadcrumbs1={t('SectionTitle.CourseDetails.breadcrumbs1')}
+        breadcrumbs2={t('SectionTitle.CourseDetails.breadcrumbs2')}
+        breadcrumbs3={t('SectionTitle.CourseDetails.breadcrumbs3')}
         breadcrumbs1Path="/"
         breadcrumbs2Path="/course"
         breadcrumbs3Path="/course-details"
-        detailsHeader={t("SectionTitle.CourseDetails.title1")}
-        detailsDescription={t("SectionTitle.CourseDetails.description")}
-        button={t("SectionTitle.CourseDetails.button")}
+        detailsHeader={t('SectionTitle.CourseDetails.title1')}
+        detailsDescription={t('SectionTitle.CourseDetails.description')}
+        button={t('SectionTitle.CourseDetails.button')}
         rating={4.5}
         ratingCount={350123}
         studentCount={1051273}
-        ratingsText={t("SectionTitle.CourseDetails.rating")}
-        studentsText={t("SectionTitle.CourseDetails.students")}
-        className={`text-center ${isArabic ? "md:text-right" : "md:text-left"}`}
+        ratingsText={t('SectionTitle.CourseDetails.rating')}
+        studentsText={t('SectionTitle.CourseDetails.students')}
+        className={`text-center ${isArabic ? 'md:text-right' : 'md:text-left'}`}
       />
 
       <div
         className={`${
-          theme === themes.dark ? "bg-dark-900" : "bg-gray-50"
+          theme === themes.dark ? 'bg-dark-900' : 'bg-gray-50'
         } py-10`}
       >
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
@@ -56,98 +56,207 @@ function CourseDetails() {
               activeTab={activeTab}
               onTabChange={setActiveTab}
               tabs={[
-                t("SectionTitle.CourseDetailsContent.tabs.overview"),
-                t("SectionTitle.CourseDetailsContent.tabs.curriculum"),
-                t("SectionTitle.CourseDetailsContent.tabs.instructor"),
-                t("SectionTitle.CourseDetailsContent.tabs.reviews"),
+                t('SectionTitle.CourseDetailsContent.tabs.overview'),
+                t('SectionTitle.CourseDetailsContent.tabs.curriculum'),
+                t('SectionTitle.CourseDetailsContent.tabs.instructor'),
+                t('SectionTitle.CourseDetailsContent.tabs.reviews'),
               ]}
               descriptionTitle={t(
-                "SectionTitle.CourseDetailsContent.description.title"
+                'SectionTitle.CourseDetailsContent.description.title'
               )}
               descriptionParagraphs={[
-                t("SectionTitle.CourseDetailsContent.description.p1"),
-                t("SectionTitle.CourseDetailsContent.description.p2"),
+                t('SectionTitle.CourseDetailsContent.description.p1'),
+                t('SectionTitle.CourseDetailsContent.description.p2'),
               ]}
               learningTitle={t(
-                "SectionTitle.CourseDetailsContent.learning.title"
+                'SectionTitle.CourseDetailsContent.learning.title'
               )}
               learningPoints={[
-                t("SectionTitle.CourseDetailsContent.learning.p1"),
-                t("SectionTitle.CourseDetailsContent.learning.p2"),
-                t("SectionTitle.CourseDetailsContent.learning.p3"),
+                t('SectionTitle.CourseDetailsContent.learning.p1'),
+                t('SectionTitle.CourseDetailsContent.learning.p2'),
+                t('SectionTitle.CourseDetailsContent.learning.p3'),
               ]}
+              curriculum={{
+                title: t('SectionTitle.CourseDetailsContent.curriculum.title'),
+                description: t(
+                  'SectionTitle.CourseDetailsContent.curriculum.description'
+                ),
+                timeOneToFour: t(
+                  'SectionTitle.CourseDetailsContent.curriculum.timeOneToFour'
+                ),
+                timeFiveToSeven: t(
+                  'SectionTitle.CourseDetailsContent.curriculum.timeFiveToSeven'
+                ),
+
+                weekOneToFourDescription: t(
+                  'SectionTitle.CourseDetailsContent.curriculum.weekOneToFourDescription'
+                ),
+                weekFiveToSevenDescription: t(
+                  'SectionTitle.CourseDetailsContent.curriculum.weekFiveToSevenDescription'
+                ),
+                weekOneToFourLearning: [
+                  t(
+                    'SectionTitle.CourseDetailsContent.curriculum.weekOneToFourLearning.0'
+                  ),
+                  t(
+                    'SectionTitle.CourseDetailsContent.curriculum.weekOneToFourLearning.1'
+                  ),
+                  t(
+                    'SectionTitle.CourseDetailsContent.curriculum.weekOneToFourLearning.2'
+                  ),
+                  t(
+                    'SectionTitle.CourseDetailsContent.curriculum.weekOneToFourLearning.3'
+                  ),
+                  t(
+                    'SectionTitle.CourseDetailsContent.curriculum.weekOneToFourLearning.4'
+                  ),
+                  t(
+                    'SectionTitle.CourseDetailsContent.curriculum.weekOneToFourLearning.5'
+                  ),
+                ],
+                weekFiveToSevenLearning: [
+                  t(
+                    'SectionTitle.CourseDetailsContent.curriculum.weekFiveToSevenLearning.0'
+                  ),
+                  t(
+                    'SectionTitle.CourseDetailsContent.curriculum.weekFiveToSevenLearning.1'
+                  ),
+                  t(
+                    'SectionTitle.CourseDetailsContent.curriculum.weekFiveToSevenLearning.2'
+                  ),
+                  t(
+                    'SectionTitle.CourseDetailsContent.curriculum.weekFiveToSevenLearning.3'
+                  ),
+                  t(
+                    'SectionTitle.CourseDetailsContent.curriculum.weekFiveToSevenLearning.4'
+                  ),
+                  t(
+                    'SectionTitle.CourseDetailsContent.curriculum.weekFiveToSevenLearning.5'
+                  ),
+                ],
+              }}
+              instructor={{
+                name: t('SectionTitle.CourseDetailsContent.instructor.name'),
+                position: t(
+                  'SectionTitle.CourseDetailsContent.instructor.position'
+                ),
+                bio: t('SectionTitle.CourseDetailsContent.instructor.bio'),
+              }}
+              reviews={{
+                title: t('SectionTitle.CourseDetailsContent.reviews.title'),
+                description: t(
+                  'SectionTitle.CourseDetailsContent.reviews.description'
+                ),
+                rate: t('SectionTitle.CourseDetailsContent.reviews.rate'),
+                numOfReviews: t(
+                  'SectionTitle.CourseDetailsContent.reviews.numOfReviews'
+                ),
+              }}
+              studentReview={{
+                title: t(
+                  'SectionTitle.CourseDetailsContent.studentReview.title'
+                ),
+                comments: t(
+                  'SectionTitle.CourseDetailsContent.studentReview.comments',
+                  { returnObjects: true }
+                ) as { rate: number; date: string; comment: string }[],
+              }}
+              studentsComments={{
+                title: t(
+                  'SectionTitle.CourseDetailsContent.studentsComments.title'
+                ),
+                leaveRate: t(
+                  'SectionTitle.CourseDetailsContent.studentsComments.leaveRate'
+                ),
+                ReviewTitle: t(
+                  'SectionTitle.CourseDetailsContent.studentsComments.ReviewTitle'
+                ),
+                writeName: t(
+                  'SectionTitle.CourseDetailsContent.studentsComments.writeName'
+                ),
+                ReviwerEmail: t(
+                  'SectionTitle.CourseDetailsContent.studentsComments.ReviwerEmail'
+                ),
+                ReviwerSummary: t(
+                  'SectionTitle.CourseDetailsContent.studentsComments.ReviwerSummary'
+                ),
+                submitReview: t(
+                  'SectionTitle.CourseDetailsContent.studentsComments.submitReview'
+                ),
+              }}
             />
+
             <CourseDetailsSidebar
               courseDetails={t(
-                "SectionTitle.CourseDetailsContent.sidebar.courseDetails"
+                'SectionTitle.CourseDetailsContent.sidebar.courseDetails'
               )}
               priceLabel={t(
-                "SectionTitle.CourseDetailsContent.sidebar.price.label"
+                'SectionTitle.CourseDetailsContent.sidebar.price.label'
               )}
               priceValue={t(
-                "SectionTitle.CourseDetailsContent.sidebar.price.value"
+                'SectionTitle.CourseDetailsContent.sidebar.price.value'
               )}
               details={[
                 {
                   icon: User,
                   label: t(
-                    "SectionTitle.CourseDetailsContent.sidebar.details.instructor.label"
+                    'SectionTitle.CourseDetailsContent.sidebar.details.instructor.label'
                   ),
                   value: t(
-                    "SectionTitle.CourseDetailsContent.sidebar.details.instructor.value"
+                    'SectionTitle.CourseDetailsContent.sidebar.details.instructor.value'
                   ),
                 },
                 {
                   icon: Clock,
                   label: t(
-                    "SectionTitle.CourseDetailsContent.sidebar.duration.label"
+                    'SectionTitle.CourseDetailsContent.sidebar.duration.label'
                   ),
                   value: t(
-                    "SectionTitle.CourseDetailsContent.sidebar.duration.value"
+                    'SectionTitle.CourseDetailsContent.sidebar.duration.value'
                   ),
                 },
                 {
                   icon: BookOpen,
                   label: t(
-                    "SectionTitle.CourseDetailsContent.sidebar.lessons.label"
+                    'SectionTitle.CourseDetailsContent.sidebar.lessons.label'
                   ),
                   value: t(
-                    "SectionTitle.CourseDetailsContent.sidebar.lessons.value"
+                    'SectionTitle.CourseDetailsContent.sidebar.lessons.value'
                   ),
                 },
                 {
                   icon: Users,
                   label: t(
-                    "SectionTitle.CourseDetailsContent.sidebar.enrolled.label"
+                    'SectionTitle.CourseDetailsContent.sidebar.enrolled.label'
                   ),
                   value: t(
-                    "SectionTitle.CourseDetailsContent.sidebar.enrolled.value"
+                    'SectionTitle.CourseDetailsContent.sidebar.enrolled.value'
                   ),
                 },
                 {
                   icon: Globe,
                   label: t(
-                    "SectionTitle.CourseDetailsContent.sidebar.language.label"
+                    'SectionTitle.CourseDetailsContent.sidebar.language.label'
                   ),
                   value: t(
-                    "SectionTitle.CourseDetailsContent.sidebar.language.value"
+                    'SectionTitle.CourseDetailsContent.sidebar.language.value'
                   ),
                 },
                 {
                   icon: Award,
                   label: t(
-                    "SectionTitle.CourseDetailsContent.sidebar.certificate.label"
+                    'SectionTitle.CourseDetailsContent.sidebar.certificate.label'
                   ),
                   value: t(
-                    "SectionTitle.CourseDetailsContent.sidebar.certificate.value"
+                    'SectionTitle.CourseDetailsContent.sidebar.certificate.value'
                   ),
                 },
               ]}
               addToCartText={t(
-                "SectionTitle.CourseDetailsContent.sidebar.actions.addToCart"
+                'SectionTitle.CourseDetailsContent.sidebar.actions.addToCart'
               )}
               buyNowText={t(
-                "SectionTitle.CourseDetailsContent.sidebar.actions.buyNow"
+                'SectionTitle.CourseDetailsContent.sidebar.actions.buyNow'
               )}
             />
           </div>
