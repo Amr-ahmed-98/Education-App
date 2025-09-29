41c5d36d84d0b50c06d68d5f226b61c3038f2a6f
Amr-ahmed-98 <67926050+Amr-ahmed-98@users.noreply.github.com>
2025-09-29 15:15:38 +0300

    Fix it and added Root File to enhance performance

---
+++ src/Root.tsx
@@ -0,0 +1,31 @@
+import { RouterProvider } from 'react-router-dom';
+import AppRouter from './routes/AppRouter';
+import { LanguageProvider } from './contexts/LanguageContext';
+import { ThemeProvider } from './contexts/ThemeContext';
+import {  useEffect, useState } from 'react';
+import LoadingScreen from './components/Errors/Loading';
+
+const Root = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <LoadingScreen />;
+  }
+
+  return (
+    <LanguageProvider>
+      <ThemeProvider>
+        <RouterProvider router={AppRouter} />
+      </ThemeProvider>
+    </LanguageProvider>
+  );
+};
+
+export default Root;
--- src/main.tsx
+++ src/main.tsx
@@ -1,35 +1,11 @@
-import {  StrictMode,useEffect, useState } from 'react';
+import {  StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import AppRouter from './routes/AppRouter';
-import { LanguageProvider } from './contexts/LanguageContext';
-import { ThemeProvider } from './contexts/ThemeContext';
+import Root from './Root';
 import './styles/index.css';
 import './i18n';
-import LoadingScreen from './components/Errors/Loading';
 
-const Root = () => {
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 1500);
-    return () => clearTimeout(timer);
-  }, []);
 
-  if (loading) {
-    return <LoadingScreen />;
-  }
-
-  return (
-    <LanguageProvider>
-      <ThemeProvider>
-        <RouterProvider router={AppRouter} />
-      </ThemeProvider>
-    </LanguageProvider>
-  );
-};
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
