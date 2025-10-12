b3ba493a69e6d17c93bc64c5547e9e5dc738b05e
eslam-mohamedl <ee.eslam355@gmail.com>
2025-10-12 07:04:14 +0300

    add page otp

---
+++ src/features/auth/api/apiVerifyOtp.ts
@@ -0,0 +1,13 @@
+import ApiClient from "@/services/ApiClient";
+
+export interface VerifyOtpPayload{
+    otp_code:string;
+    password: string; 
+}
+export interface VerifyOtpResponse{
+    message:string;
+}
+
+export function verifyOtp(payload:VerifyOtpPayload) : Promise<VerifyOtpResponse>{
+    return ApiClient.post<VerifyOtpResponse>("/auth/resetPassword", payload);
+}
